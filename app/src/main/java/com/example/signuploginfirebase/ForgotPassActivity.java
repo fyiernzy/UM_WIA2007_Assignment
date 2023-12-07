64ae880ae5a3b6d692ce31fadb69094cf6bfb465
Vyan <vyanleehuijie@gmail.com>
2023-12-08 05:48:36 +0800

    Initial Commit

---
+++ app/src/main/java/com/example/signuploginfirebase/ForgotPassActivity.java
@@ -0,0 +1,70 @@
+package com.example.signuploginfirebase;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ForgotPassActivity extends AppCompatActivity {
+
+    private TextView loginRedirectText;
+    private FirebaseAuth auth;
+    private Button forgotPassButton;
+
+    private EditText forgotPassEmail;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_pass);
+
+        auth = FirebaseAuth.getInstance();
+        loginRedirectText = findViewById(R.id.loginRedirectText);
+        forgotPassButton = findViewById(R.id.send_link_button);
+        forgotPassEmail = findViewById(R.id.forgotPass_email);
+
+
+        loginRedirectText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ForgotPassActivity.this, LoginActivity.class));
+            }
+        });
+
+        forgotPassButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String userEmail = forgotPassEmail.getText().toString();
+
+                if(TextUtils.isEmpty(userEmail) && !Patterns.EMAIL_ADDRESS.matcher(userEmail).matches()){
+                    Toast.makeText(ForgotPassActivity.this, "Enter your registered email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                auth.sendPasswordResetEmail(userEmail).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(ForgotPassActivity.this, "Check your email", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(ForgotPassActivity.this, LoginActivity.class));
+                        } else {
+                            Toast.makeText(ForgotPassActivity.this, "Unable to send, failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/signuploginfirebase/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.signuploginfirebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button firstloginButton, firstsignupButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        firstloginButton = findViewById(R.id.first_login_button);
+        firstsignupButton = findViewById(R.id.first_signup_button);
+
+
+        firstloginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        });
+
+        firstsignupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+            }
+        });
+    }
+}
